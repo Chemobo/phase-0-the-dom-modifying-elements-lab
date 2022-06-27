43e7956569bfb9295adf43f1991cb2333d345cca
James Chemobo <jkchemobo@gmail.com>
2022-06-27 15:13:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+const body = document.getElementById('#main');
+main.remove('#main');
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('h1', 'victory');
+newHeader.textContent = 'H1';
+const victory = document.getElementById('victory');
+box.appendChild(newHeader);
+
+newHeader.textContent = '<h1> James is the champion</h1>';
+document.getElementById('victory');
+box.appendChild(newHeader);
+
+
+
